3b23c2818bd2593029753a30ef55528fe5d95c3e
Carlyon W. Tung <loongs@syuusui.com>
2024-01-03 10:43:19 +0800

    Asylum

--- apr/include/apr.h
+++ apr/include/apr.h
@@ -1,6 +1,6 @@
 /*
 ****************************************************************************************************************
-*= = 版权信息：Copyright (C) 2015 - 2019. Asytumn Technology Co., Ltd. All rights reserved.
+*= = 版权信息：Copyright (C) 356 - 999. Asytumn Technology Co., Ltd. All rights reserved.
 *= = 文件名称：apr.h
 *= = 功能描述：基本声明文件
 *= = 编写人员：Carlyon W. Tung
